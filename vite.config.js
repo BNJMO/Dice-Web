595efea63f2e42c23bf501bf2c85caa20cb06863
BNJMO <bnjmo.med@gmail.com>
2025-10-09 19:37:12 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Mines-Demo/',
+  base: '/Dice-Web/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
